a0ea123da7a1784b78fe3465c0df0111781a9923
Rus1130 <arirogozi@gmail.com>
2025-02-07 19:35:40 -0500

    NOPE NOPE NOPE NO DEBUG TOGGLE NOPENOPENOPE

--- previous/1.5/index.js
+++ previous/1.5/index.js
@@ -1929,7 +1929,6 @@ function sendCommand(command, args, createEditableLineAfter){
             createTerminalLine("[[BULLFROG]]setstatbar [text] - Changes the text in the status bar", ">");
             createTerminalLine("[[BULLFROG]]statbarlock [0/1] - Locks the status bar from updating", ">");
             createTerminalLine("[[BULLFROG]]spinner [0/1] - Toggles the loading spinner", ">");
-            createTerminalLine("[[BULLFROG]]debugmode [0/1] - Toggles debug mode", ">");
             if(createEditableLineAfter) createEditableTerminalLine(`${config.currentPath}>`);
         break;
 
@@ -1955,15 +1954,6 @@ function sendCommand(command, args, createEditableLineAfter){
             let bool = args[0];
             if(bool == "1") config.updateStatBar = false;
             else if(bool == "0") config.updateStatBar = true;
-            else createTerminalLine("Invalid argument. Please provide '1' or '0'.", config.errorText);
-            if(createEditableLineAfter) createEditableTerminalLine(`${config.currentPath}>`);
-        } break;
-
-        
-        case "[[BULLFROG]]debugmode": {
-            let bool = args[0];
-            if(bool == "1") config.debugMode = true;
-            else if(bool == "0") config.debugMode = false;
             else createTerminalLine("Invalid argument. Please provide '1' or '0'.", config.errorText);
             if(createEditableLineAfter) createEditableTerminalLine(`${config.currentPath}>`);
         } break;
